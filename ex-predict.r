844fd287692e38dc3550455cf55ebe0e00a02392
Gus <gusmairs@mac.com>
2019-08-23 11:06:17 -0700

    better validation scheme now

--- ex-predict.r
+++ ex-predict.r
@@ -8,7 +8,7 @@
 libraries <- c(
     'caret', 'e1071', 'readr', 'plyr'
 )
-invisible(lapply(libraries, library, character.only = TRUE, quietly = True))
+invisible(lapply(libraries, library, character.only = TRUE, quietly = TRUE))
 
 # %% Load and subset data, eliminate the 100 out of 160 features with no data
 #
@@ -29,17 +29,13 @@ train_full <- train_raw[, !na_idx]  # 60 features
 test <- test_raw[, !na_idx]
 features <- 8:59
 
-# %% Set up train dataset and 5 small validation folds
+# %% Set up train dataset and a 20x5 index matrix for 5 validation folds
 #
 set.seed(78)
 train_idx <- caret::createDataPartition(train_full$id, p = 0.95, list = FALSE)
 train <- train_full[train_idx, ]
 validate <- train_full[-train_idx, ]
-val_idx <- replicate(5, sample(validate$id, 20))
-for (i in 1:5) {
-    fold <- validate[which(validate$id %in% val_idx[, i]), ]
-    do.call('<-', list(paste0('cv_', i), fold))
-}
+val_matrix <- replicate(5, sample(validate$id, 20))
 
 # %% Run PCA to transform train into matrix explaining 95% of the variance
 #
@@ -54,32 +50,20 @@ conf_matrix <- caret::confusionMatrix(svm_predict, train$label)
 print(conf_matrix$table)
 print(conf_matrix$overall['Accuracy'])
 
-# %% Run the SVM prediction on 3 of the validation sets
+# %% Run the SVM prediction the 5 validation sets
 #
-
-val_test <- function(prep, model, fold) {
-    fold_pca <- predict(prep, fold[, features])
+val_test <- function(prep, model, x, y) {
+    fold_pca <- predict(prep, x)
     fold_pred <- predict(model, newdata = fold_pca)
-    caret::confusionMatrix(fold_pred, fold$label)$overall['Accuracy']
+    caret::confusionMatrix(fold_pred, y)$overall['Accuracy']
 }
 
-acc_1 <- val_test(pca_fit, svm_fit, cv_1)
-
-
-
-cv_1_pca <- predict(pca_fit, cv_1[, features])
-cv_1_pred <- predict(svm_fit, newdata = cv_1_pca)
-acc_1 <- confusionMatrix(cv_1_pred, cv_1$label)$overall['Accuracy']
-
-cv_2_pca <- predict(pca_fit, cv_2[, features])
-cv_2_pred <- predict(svm_fit, newdata = cv_2_pca)
-acc_2 <- confusionMatrix(cv_2_pred, cv_2$label)$overall['Accuracy']
-
-cv_3_pca <- predict(pca_fit, cv_3[, features])
-cv_3_pred <- predict(svm_fit, newdata = cv_3_pca)
-acc_3 <- confusionMatrix(cv_3_pred, cv_3$label)$overall['Accuracy']
-
-cbind(acc_1, acc_2, acc_3)
+acc <- list()
+for (i in 1:5) {
+    fold <- validate[which(validate$id %in% val_matrix[, i]), ]
+    acc[i] <- val_test(pca_fit, svm_fit, fold[, features], fold[, 'label'])
+}
+print(cbind(acc))
 
 # %% Transform the test sample and run the model to predict final
 #
